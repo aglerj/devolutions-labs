b1b25a88ab6e972d32bc0fee875bc7166cc2fdd0
Joe Agler <21351031+aglerj@users.noreply.github.com>
2026-01-14 22:30:56 -0500

    Update build.ps1

--- powershell/build.ps1
+++ powershell/build.ps1
@@ -1,6 +1,12 @@
 #Requires -RunAsAdministrator
 #Requires -PSEdition Core
 
+[CmdletBinding()]
+param(
+    # When specified, create a 2nd Gateway VM (GW2) for Gateway Farm testing
+    [switch] $EnableGatewayFarm
+)
+
 . .\common.ps1
 
 Write-Host "Creating $LabPrefix lab..."
@@ -21,9 +27,15 @@ Write-Host "Creating GW VM..."
 $TimeGW = Measure-Command { .\gw_vm.ps1 }
 Write-Host "GW VM creation time: $TimeGW"
 
+$TimeGW2 = [TimeSpan]::Zero
+if ($EnableGatewayFarm) {
 Write-Host "Creating GW2 VM..."
 $TimeGW2 = Measure-Command { .\gw_vm2.ps1 }
 Write-Host "GW2 VM creation time: $TimeGW2"
+}
+else {
+    Write-Host "Skipping GW2 VM creation (run with -CreateGW2 to enable)."
+}
 
 Write-Host "Creating RDM VM..."
 $TimeRDM = Measure-Command { .\rdm_vm.ps1 }
